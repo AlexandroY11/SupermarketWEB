b992c3fcb9c6b6465d2a06037db225429ecb4685
AlexandroY11 <alexandroyule@gmail.com>
2023-11-10 23:37:22 -0500

    Update Product Model, Create and Edit Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,17 +5,14 @@ namespace SupermarketWEB.Models
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [Column(TypeName = "decimal(6.2)")]
+        [Column(TypeName = "decimal(6,0)")]
         public decimal Price { get; set; }
 
         public int Stock { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        //public ICollection<Category>? Categories { get; set; } = default!;
-
-
-
+        //public Category Category { get; set; }
+        public ICollection<Category>? Categories { get; set; } = default!;
     }
 }

3fccb2b237d36fe1021e1da831772c1fe024faa5
Alexandro Yule <alexandroyule@gmail.com>
2023-11-11 15:27:16 -0500

    Create Paymodes

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,7 +12,7 @@ namespace SupermarketWEB.Models
 
         public int Stock { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        //public ICollection<Category>? Categories { get; set; } = default!;
+        //public Category Category { get; set; }
+        public ICollection<Category>? Categories { get; set; } = default!;
     }
 }

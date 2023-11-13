9ba1a401a33503d5f4096304f4a32d3b534e8511
AlexandroY11 <alexandroyule@gmail.com>
2023-11-13 00:14:31 -0500

    Styling Index Page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let activeImage = 0;
+const slider = document.getElementById('image-slider');
 
-// Write your JavaScript code.
+function changeImage(index) {
+    activeImage = index;
+    updateSlider();
+    console.log("yuju");
+}
+
+function updateSlider() {
+    const newPosition = activeImage * slider.clientWidth;
+    slider.scrollLeft = newPosition;
+}
